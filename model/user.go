e55aa5c8537a3505270c84feaf3e29e9138b1e7c
Restu Adi Wahyujati <adijati1029@gmail.com>
2025-06-27 19:15:17 +0700

    [FIX]model change

--- model/user.go
+++ model/user.go
@@ -6,13 +6,14 @@ import (
 	"github.com/google/uuid"
 )
 
-type Employee struct {
+type User struct {
 	ID 			uuid.UUID		`db:"id" json:"id"`
 	Name 		string			`db:"name" json:"name"`
 	Email 		string			`db:"email" json:"email"`
 	Phone 		string			`db:"phone" json:"phone"`
 	Password	string			`db:"password" json:"-"`
 	Address 	string			`db:"address" json:"address"`
+	Role		string			`db:"role" json:"role"`
 	Created_At 	time.Time		`db:"created_at" json:"created_at"`
 	Updated_At 	time.Time		`db:"updated_at" json:"updated_at"`
 }
--- utils/model/jwt_model.go
+++ utils/model/jwt_model.go
@@ -1,1 +1,12 @@
 package model
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type JWTPayloadClaim struct {
+	jwt.RegisteredClaims
+	UserId	uuid.UUID
+	Role	string
+}
